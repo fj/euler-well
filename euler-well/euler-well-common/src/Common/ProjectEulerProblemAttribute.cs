ac2749dcd8abaaa87b7708566c6337c8f0fa790f
John Feminella <johnf.public@distilledb.com>
2009-02-25 09:29:45 -0500

    Adds basic solution structure for EulerWell. Adds Common project containing useful constructs.

---
+++ euler-well/euler-well-common/src/Common/ProjectEulerProblemAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DistilledB.EulerWell.Common
+{
+  [AttributeUsage(AttributeTargets.Class)]
+  public class ProjectEulerProblemAttribute : System.Attribute
+  {
+    private int id;
+
+    public ProjectEulerProblemAttribute(int id) {
+      this.id = id;
+    }
+
+    public string Name { get; set; }
+    public int Id { get { return id; } }
+    public string Description { get; set; }
+  }
+}
