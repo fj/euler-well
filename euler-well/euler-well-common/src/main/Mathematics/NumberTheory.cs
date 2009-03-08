ae6a4f0350ef86ad3a8cdf2066f5788c1c986dbf
John Feminella <johnf.public@distilledb.com>
2009-03-08 14:44:49 -0400

    Small reorganization of NumberTheory.

--- euler-well/euler-well-common/src/main/Mathematics/NumberTheory.cs
+++ euler-well/euler-well-common/src/main/Mathematics/NumberTheory.cs
@@ -5,7 +5,8 @@ namespace DistilledB.EulerWell.Mathematics {
     /// Returns true if specified number has no prime factors other than itself.
     /// </summary>
     /// <remarks>
-    /// This method uses naive trial division to test each number in turn, beginning with 2 and then testing all odd numbers.
+    /// This method uses naive trial division to test each number in turn, beginning with 2 and then testing all
+    /// odd numbers up to the square root of the specified number.
     /// </remarks>
     /// <param name="n">the number to test</param>
     /// <returns></returns>
@@ -14,12 +15,12 @@ namespace DistilledB.EulerWell.Mathematics {
       // An even number is prime only if that number is 2.
       if (n % 2 == 0) return n == 2;
 
-      var isComposite = false;
-      var x = 3;
-
       // If this number had a factor greater than sqrt(n), then it must have had a factor less than sqrt(n), and we
       // would have found it already.
       var limit = System.Math.Sqrt(n);
+
+      var isComposite = false;
+      var x = 3;
 
       // Test each factor up to and including the limit.
       for (; x <= limit && !isComposite; x += 2)
